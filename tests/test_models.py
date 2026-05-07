af99fa752a34c676db9ba347bc097dbb21463afc
Lukasz Jachym <lukasz.jachym@gmail.com>
2026-05-07 16:01:01 +0700

    fix(models): extend supported currency pairs

--- tests/test_models.py
+++ tests/test_models.py
@@ -18,6 +18,21 @@ class TestWalutomatOrder(unittest.TestCase):
         pair = OrderCurrencyPair(base=OrderCurrencyEnum.EUR, counter=OrderCurrencyEnum.PLN)
         self.assertEqual(str(pair), 'EURPLN')
 
+    def test_order_currency_enum_supports_requested_pairs(self):
+        pairs = [
+            'EURGBP', 'EURUSD', 'EURCHF', 'EURPLN', 'GBPUSD', 'GBPCHF', 'GBPPLN',
+            'USDCHF', 'USDPLN', 'CHFPLN', 'EURSEK', 'EURNOK', 'EURDKK', 'EURCZK',
+            'CZKPLN', 'DKKPLN', 'NOKPLN', 'SEKPLN', 'AUDPLN', 'CADPLN', 'CNYPLN',
+            'HKDPLN', 'HUFPLN', 'ILSPLN', 'JPYPLN', 'MXNPLN', 'NZDPLN', 'RONPLN',
+            'SGDPLN', 'TRYPLN', 'ZARPLN', 'EURAUD', 'EURCAD', 'EURCNY', 'EURHKD',
+            'EURHUF', 'EURILS', 'EURJPY', 'EURMXN', 'EURNZD', 'EURRON', 'EURSGD',
+            'EURTRY', 'EURZAR',
+        ]
+
+        for pair in pairs:
+            with self.subTest(pair=pair):
+                self.assertEqual(str(OrderCurrencyEnum(pair)), pair)
+
     def test_parsing_nanoseconds(self):
         datetime_str = '2022-08-03T09:50:16.692380437Z'
         raw_order = read_fixture('order_result.json')
